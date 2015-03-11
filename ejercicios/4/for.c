af45f03f46e0ee2fc1fe23b76d5439c6bae04bd3
VaneChipi <vchipirras@hotmail.com>
2015-03-11 18:51:20 +0100

    Subido ejercicio 4 'for.c'

--- ejercicios/4/for.c
+++ ejercicios/4/for.c
@@ -5,9 +5,15 @@
 
 /* Bibliotecas a incluir */
 
+#include <stdio.h>
+
 void main()
 {
 	/* Declaración de variables */
+	int len = 0;
+	char cadena[8] = "cadenas";
 
 	/* Código usando condicional while */
+	for (len = 0; cadena[len] != '\0'; len++);
+	printf("Tamaño de la cadena es: %d\n", len);
 }
